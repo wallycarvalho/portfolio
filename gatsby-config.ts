b046c0c215eb34964ca87ac5c71885f2e7b33e1c
Allyson Alves <allyson.carvalho@gmail.com>
2024-03-13 12:14:50 +0000

    netlify addition

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,14 +9,21 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-decap-cms"
+    , {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }]
 };
 
 export default config;
---
+++ src/pages/about.tsx
@@ -0,0 +1,26 @@
+// Step 1: Import React
+import * as React from 'react'
+import type { HeadFC, PageProps } from "gatsby"
+import Layout from '../components/layout'
+
+// Step 2: Define your component
+const AboutPage: React.FC<PageProps> = () => {
+    return (
+        <main>
+            <Layout pageTitle="About page">Header with info</Layout>
+            <br />
+            <h1>About Me</h1>
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        </main>
+    )
+}
+
+export const Head: HeadFC = () => (
+    <>
+        <title>About Me</title>
+        <meta name="description" content="Your description" />
+    </>
+)
+
+// Step 3: Export your component
+export default AboutPage
