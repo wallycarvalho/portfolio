b14f0018de4504b8db823025492ee1fa3de0a4eb
Allyson Alves <allyson.carvalho@gmail.com>
2024-03-20 11:28:36 +0000

    lint: addressing exceeding line in config file

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,7 +5,8 @@ const config: GatsbyConfig = {
     title: 'allyson-homepage',
     siteUrl: 'https://www.yourdomain.tld',
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // More easily incorporate content into your
+  // pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
